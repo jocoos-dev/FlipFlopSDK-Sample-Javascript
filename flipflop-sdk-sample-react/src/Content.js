88ff6746f165ed6c8daf8dea8c1f7ec99ed2d332
jocoos-dev <dev@jocoos.com>
2022-07-04 16:40:08 +0900

    remove deprecated code

--- flipflop-sdk-sample-react/src/Content.js
+++ flipflop-sdk-sample-react/src/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { VideoList, VideoViewer, VideoStreamer, Logout, MobileViewer } from './pages'
+import { VideoList, VideoViewer, VideoStreamer } from './pages'
 import { Route } from 'react-router-dom'
 
 class Content extends React.Component {
@@ -7,10 +7,8 @@ class Content extends React.Component {
     return (
       <div className="content">
         <Route exact path="/" component={VideoList} />
-        <Route exact path="/logout" component={Logout} />
         <Route exact path="/videos/:id" component={VideoViewer} />
         <Route exact path="/videos" component={VideoStreamer} />
-        <Route exact path="/mv" component={MobileViewer} />
       </div>
     )
   }
